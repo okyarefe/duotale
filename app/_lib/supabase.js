2bc64aa2fa5b9e03ea8e23d36adf87c14fc1f4b4
efe okyar <f.efe.okyar@gmail.com>
2024-07-15 01:40:57 +0300

    revert sp url

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -1,4 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = process.env.SUPABASE_URL;
-export const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
