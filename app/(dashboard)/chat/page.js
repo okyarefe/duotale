67bc9e11b65e1da9d5b8fdda38fe08939cbc00ac
efe okyar <f.efe.okyar@gmail.com>
2024-07-31 18:53:21 +0300

    final

--- app/(dashboard)/chat/page.js
+++ app/(dashboard)/chat/page.js
@@ -22,7 +22,7 @@ const ChatPage = async () => {
 
     return (
       <div>
-        <Chat token={userToken} userId={userId} />
+        <Chat token={userToken} />
       </div>
     );
   } catch (error) {
@@ -32,30 +32,3 @@ const ChatPage = async () => {
 };
 
 export default ChatPage;
-
-// import Chat from "../../../components/Chat";
-// import { currentUser } from "@clerk/nextjs/server";
-// import { getUserTokenById } from "../../_lib/data-service";
-
-// const ChatPage = async () => {
-//   // get user id
-//   const user = await currentUser();
-//   const userId = user.id;
-//   console.log("CURRENT USER ID", userId);
-//   if (!userId) return <h1>Not authorized</h1>;
-
-//   const userToken = await getUserTokenById(userId);
-
-//   // if (!userTokenObj || userTokenObj.length === 0 || !userTokenObj[0].token) {
-//   //   console.error("Token not found for user ID:", userId);
-//   //   return <h1>User token not found</h1>;
-//   // }
-
-//   return (
-//     <div>
-//       <Chat token={userToken} userId={userId}></Chat>
-//     </div>
-//   );
-// };
-
-// export default ChatPage;
