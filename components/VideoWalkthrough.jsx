0a69717a3acc85ea81203d59f6e5371123dcab1e
efe okyar <f.efe.okyar@gmail.com>
2024-09-03 13:07:40 +0300

    videowalkthrough old version

--- components/VideoWalkthrough.jsx
+++ components/VideoWalkthrough.jsx
@@ -1,35 +1,7 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 
 const VideoWalkthrough = () => {
-  useEffect(() => {
-    const videoElement = document.getElementById("walkthroughVideo");
-
-    // Attempt to play the video immediately
-    videoElement.play().catch((error) => {
-      console.log("Auto-play was prevented by the browser.", error);
-    });
-
-    // Unmute the video when the user hovers over the screen
-    const handleMouseMove = () => {
-      videoElement.muted = false;
-      videoElement.play().catch((error) => {
-        console.log("Auto-play was prevented by the browser.", error);
-      });
-
-      // Remove the event listener after unmuting to prevent repeated triggers
-      document.removeEventListener("mousemove", handleMouseMove);
-    };
-
-    // Listen for mouse movement to unmute the video
-    document.addEventListener("mousemove", handleMouseMove);
-
-    return () => {
-      // Clean up event listener when the component unmounts
-      document.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, []);
-
   return (
     <div className="video-container">
       <video
