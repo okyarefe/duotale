bc2add87e1de06eb1c2e5db548ffb1c2f3bb28ea
efe okyar <f.efe.okyar@gmail.com>
2024-09-03 13:43:54 +0300

    landinpage video enable sound on click

--- app/page.js
+++ app/page.js
@@ -3,13 +3,15 @@ import VideoWalkthrough from "@/components/VideoWalkthrough";
 
 const HomePage = () => {
   return (
-    <div className="hero bg-base-200 flex flex-col">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold special leading-tight tracking-wide">
+    <div className="hero bg-base-200 flex flex-col ">
+      <div className="hero-content text-center flex flex-col">
+        {" "}
+        <div className="heading-div w-full">
+          <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold special leading-tight tracking-wide ">
             LEARN LANGUAGES THE FUN WAY!
           </h1>
-
+        </div>
+        <div className="max-w-md ">
           <h2 className="py-6 text-lg md:text-xl lg:text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 leading-tight tracking-wide">
             Create your own stories and unlock a new world where you learn
             languages through fun and engaging stories in dual language format.
@@ -20,7 +22,7 @@ const HomePage = () => {
           </Link>
         </div>
       </div>
-      <div className="hero-content">
+      <div className="hero-content ">
         <VideoWalkthrough />
       </div>
     </div>
--- components/VideoWalkthrough.jsx
+++ components/VideoWalkthrough.jsx
@@ -1,17 +1,37 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 
 const VideoWalkthrough = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handlePlayWithSound = () => {
+    const videoElement = document.getElementById("walkthroughVideo");
+    videoElement.muted = false;
+    videoElement
+      .play()
+      .then(() => {
+        setIsPlaying(true);
+      })
+      .catch((error) => {
+        console.log("Play failed:", error);
+      });
+  };
+
   return (
     <div className="video-container">
+      {!isPlaying && (
+        <div className="overlay" onClick={handlePlayWithSound}>
+          <p className="overlay-text">Click the video to enable sound</p>
+        </div>
+      )}
       <video
         id="walkthroughVideo"
         autoPlay
         loop
         muted
-        preload="auto"
         className="video"
         style={{ width: "100%", height: "100%" }}
+        onClick={handlePlayWithSound}
       >
         <source src="/walkthroughfinal2.mp4" type="video/mp4" />
         Your browser does not support the video tag.
