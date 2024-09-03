17a875337af0c4ba0c35416c6f06981d371ab497
efe okyar <f.efe.okyar@gmail.com>
2024-09-03 12:48:50 +0300

    video autoplay-mute fixed

--- components/VideoWalkthrough.jsx
+++ components/VideoWalkthrough.jsx
@@ -1,11 +1,33 @@
+"use client";
 import React from "react";
+import { useEffect } from "react";
 
 const VideoWalkthrough = () => {
+  useEffect(() => {
+    const videoElement = document.getElementById("walkthroughVideo");
+
+    // Play the video
+    videoElement
+      .play()
+      .then(() => {
+        // Unmute after a short delay
+        setTimeout(() => {
+          videoElement.muted = false;
+        }, 500); // Adjust delay as needed
+      })
+      .catch((error) => {
+        console.log("Auto-play was prevented by the browser.", error);
+      });
+  }, []);
+
   return (
     <div className="video-container">
       <video
+        id="walkthroughVideo"
         autoPlay
         loop
+        muted
+        preload="auto"
         className="video"
         style={{ width: "100%", height: "100%" }}
       >
