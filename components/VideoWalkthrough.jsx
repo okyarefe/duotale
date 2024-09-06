3f5e707df8d90a1f3f6d19a3566a4f0fac3b39dc
efe okyar <f.efe.okyar@gmail.com>
2024-09-06 16:04:02 +0300

    landing page walkthorugh video updated

--- components/VideoWalkthrough.jsx
+++ components/VideoWalkthrough.jsx
@@ -33,7 +33,7 @@ const VideoWalkthrough = () => {
         style={{ width: "100%", height: "100%" }}
         onClick={handlePlayWithSound}
       >
-        <source src="/walkthroughfinal2.mp4" type="video/mp4" />
+        <source src="/finalelanding.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
