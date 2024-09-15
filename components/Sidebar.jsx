8f1bdb7e68a3209fcbfad937bdf914c40d4a082c
efe okyar <f.efe.okyar@gmail.com>
2024-09-15 15:10:28 +0300

    mobile screen sidebar bg color

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -5,7 +5,7 @@ import SignOutButton from "../components/SignOutButton";
 
 const Sidebar = () => {
   return (
-    <div className="px-4 w-80 min-h-full py-12 grid grid-rows-[auto-1fr-1auto] ">
+    <div className="px-4 w-80 min-h-full py-12 grid grid-rows-[auto-1fr-1auto] bg-slate-800 ">
       <SidebarHeader></SidebarHeader>
       <NavLinks></NavLinks>
       <SignOutButton></SignOutButton>
