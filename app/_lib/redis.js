e22d03d4a3dee4d4f6d5d7ec6563d5e3a5cc326b
efe okyar <f.efe.okyar@gmail.com>
2024-09-15 13:00:46 +0300

    redis url fixed

--- app/_lib/redis.js
+++ app/_lib/redis.js
@@ -7,7 +7,7 @@ const redisUrl =
     : process.env.RENDER_REDIS_URL;
 
 const client = createClient({
-  url: process.env.RENDER_REDIS_EXTERNAL_URL,
+  url: redisUrl,
 });
 //process.env.RENDER_REDIS_EXTERNAL_URL
 // process.env.RENDER_REDIS_URL
