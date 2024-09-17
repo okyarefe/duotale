7893b31822ec33524e630b47096b9518977c6668
efe okyar <f.efe.okyar@gmail.com>
2024-09-17 22:07:01 +0300

    spinner updated

--- components/Spinner.jsx
+++ components/Spinner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Spinner = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-green-400 to-blue-500">
+    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-blue-300 to-blue-500">
       <div className="relative w-24 h-24">
         {[0, 1, 2].map((index) => (
           <div
@@ -13,8 +13,8 @@ const Spinner = () => {
                           index === 0
                             ? "left-0 animation-delay-0"
                             : index === 1
-                            ? "left-8 top-8 animation-delay-150"
-                            : "left-16 animation-delay-300"
+                            ? "left-8 top-12 animation-delay-150"
+                            : "left-16 animation-delay-500"
                         }`}
             style={{
               animationDuration: "0.6s",
