de6b42ec8998dadec7485443fcd59d12f636d176
efe okyar <f.efe.okyar@gmail.com>
2024-09-18 18:34:09 +0300

    stripe key

--- app/(dashboard)/gettokens/page.js
+++ app/(dashboard)/gettokens/page.js
@@ -10,9 +10,9 @@ import Spinner from "@/components/Spinner";
 // Make sure to call loadStripe outside of a component’s render to avoid
 // recreating the Stripe object on every render.
 // This is your test publishable API key.
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
-);
+// const stripePromise = loadStripe(
+//   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+// );
 
 export default function App() {
   return <h1>Not available yet</h1>;
